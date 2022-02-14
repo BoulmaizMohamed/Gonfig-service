a0a7ae8aa87b92f9bc8c1d767376ef478e2d512f
mohamed boulmaiz <mohamed.boulmaiz@univ-constantine2.dz>
2022-02-14 17:52:25 +0100

    Config service

---
+++ src/test/java/AL/Project/Gonfigservice/GonfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package AL.Project.Gonfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
